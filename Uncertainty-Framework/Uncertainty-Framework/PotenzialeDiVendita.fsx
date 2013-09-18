9bd6eb25b546c78d5e1671a195bfea0cf98be0f8
Leonardo Bartoloni <pq@calculon>
2013-09-18 16:54:18 +0200

    Aggiunte variabili per fan (basato su eta') di Justin Bieber e Chopra

--- Uncertainty-Framework/Uncertainty-Framework/PotenzialeDiVendita.fsx
+++ Uncertainty-Framework/Uncertainty-Framework/PotenzialeDiVendita.fsx
@@ -17,8 +17,10 @@ type Sesso =
     | Maschio
     | Femmina
 
+let totpersoneusa = 304280000
+
 let sesso,eta =
-    let totpersoneusa = 304280
+    let totpersoneusa = totpersoneusa/1000 // i numeri sono in migliaia
     let getsessoeta n =
         let demograficacumulativa =
             let demograficausa =
@@ -68,15 +70,15 @@ let sesso,eta =
     }
 
 
-Dist.getSampleSeq (getDist sesso) (gen()) |> Seq.take 3042800 |> Seq.countBy (fun x -> match x with Maschio -> "Maschio"| Femmina -> "Femmina" ) |> Chart.Column
-Dist.getSampleSeq (getDist eta) (gen()) |> Seq.take 3042800 |> Seq.countBy (fun x ->x) |> Chart.Column
+//Dist.getSampleSeq (getDist sesso) (gen()) |> Seq.take 3042800 |> Seq.countBy (fun x -> match x with Maschio -> "Maschio"| Femmina -> "Femmina" ) |> Chart.Column
+//Dist.getSampleSeq (getDist eta) (gen()) |> Seq.take 3042800 |> Seq.countBy (fun x ->x) |> Chart.Column
 
 let contaPerEta s = Seq.countBy (fun (sesso,eta) -> eta) s
 
-Dist.getSampleSeq (getDist (rndvar{let! s = sesso in let! e =eta in return dist{return s,e} }) ) (gen()) |>
-    Seq.take 1000000 |>
-    Seq.groupBy fst |>
-    Seq.map (fun (sesso,sequenza) -> contaPerEta sequenza|> Seq.sort |>Chart.Column)|>Chart.Combine //match sesso with Maschio -> "maschio"| Femmina -> "femmina") |>
+//Dist.getSampleSeq (getDist (rndvar{let! s = sesso in let! e =eta in return dist{return s,e} }) ) (gen()) |>
+//    Seq.take 1000000 |>
+//    Seq.groupBy fst |>
+//    Seq.map (fun (sesso,sequenza) -> contaPerEta sequenza|> Seq.sort |>Chart.Column)|>Chart.Combine //match sesso with Maschio -> "maschio"| Femmina -> "femmina") |>
     
     (*dati inventati
 let etaprobcellulareM = [ 5,0.0; 10,0.02; 15,0.28; 20,0.8; 25,0.9; 30,0.9; 35, 0.9; 40, 0.9; 45,0.85; 50,0.8;55,0.75;60,0.6;65,0.3;70,0.2;75,0.1;80,0.0;85,0.0;10000,0.0]
@@ -107,7 +109,7 @@ let smartphone = rndvar {
     return dist {
         let! u = Dist.uniform
         let prob = match e with
-                    | e when e < 18 -> 0.063 * p_smartphone / p_bambino
+                    | e when e < 18 -> (0.063 * p_smartphone / p_bambino) * (float e) / 8.5
                     | e when e < 25 -> 0.79
                     | e when e < 35 -> 0.81
                     | e when e < 45 -> 0.69
@@ -148,8 +150,8 @@ let rec etaSmartphone () =
             return e   
     }
 
-Dist.getSampleSeq ( smartphoneeta 60 100) (gen()) |> Seq.take 304280 |> Seq.countBy (fun (x:bool) -> if x then "Hanno lo smartphone" else "Non ce l'hanno" ) |> Chart.Pie
-Dist.getSampleSeq ( etaSmartphone ()) (gen()) |> Seq.take 304280 |> Seq.countBy (fun x -> x ) |> Chart.Column
+//Dist.getSampleSeq ( smartphoneeta 60 100) (gen()) |> Seq.take 304280 |> Seq.countBy (fun (x:bool) -> if x then "Hanno lo smartphone" else "Non ce l'hanno" ) |> Chart.Pie
+//Dist.getSampleSeq ( etaSmartphone ()) (gen()) |> Seq.take 304280 |> Seq.countBy (fun x -> x ) |> Chart.Column
 
 
 let stressLevel = rndvar  {
@@ -175,19 +177,90 @@ let stressed = rndvar {
     }
 }
 
-let compraApplicazione = rndvar {
+//let propensioneAllAcquisto = rndvar {
+//    let rec arrotonda_percentuale m v = dist {
+//        let! normale = Dist.normal m v
+//        if normale > 0.0 then return normale else let! ar = arrotonda_percentuale m v in return ar
+//    }
+//    return arrotonda_percentuale 0.1 0.1
+//}
+
+let fanChopra = rndvar {
+    let! eta = eta
+    let percentuale = 1000000.0 / (float totpersoneusa)
+    return dist {
+        let! u = Dist.uniform
+        let p = match eta with 
+                | e when e < 10 -> 0.0
+                | e when e < 18 -> 0.0004
+                | e when e < 20 -> 0.001
+                | e when e < 25 -> 0.002
+                | e when e < 30 -> 0.0032
+                | e when e < 35 -> 0.004
+                | e when e < 45 -> 0.005
+                | e when e < 55 -> 0.005
+                | e when e < 65 -> 0.004
+                | _             -> 0.003
+        return u < p
+    }
+}
+//Dist.getSampleSeq (getDist fanChopra) (gen())|> Seq.take 1000000 |> Seq.filter (function None -> false | _ -> true)  |> Seq.countBy (fun x ->2 * ( x.Value /2) ) |> Chart.Column
+
+let fanBieber = rndvar {
+    let! eta = eta
+    return dist {
+        let! u = Dist.uniform
+        let p = match eta with 
+                | e when e < 5 -> 0.001
+                | e when e < 8 -> 0.05
+                | e when e < 15 -> 0.5
+                | e when e < 20 -> 0.4
+                | e when e < 25 -> 0.2
+                | e when e < 30 -> 0.1
+                | e when e < 35 -> 0.05
+                | e when e < 45 -> 0.01
+                | e when e < 55 -> 0.005
+                | e when e < 65 -> 0.001
+                | _             -> 0.001
+        return u < p
+        //return if u < p then Some eta else None
+    }
+}
+//Dist.getSampleSeq (getDist fanBieber) (gen())|> Seq.take 1000000 |> Seq.filter (function None -> false | _ -> true)  |> Seq.countBy (fun x ->2 * ( x.Value /2) ) |> Chart.Column
+//Dist.getSampleSeq (getDist fanBieber) (gen())|> Seq.take 1000000 |> Seq.average
+
+let scaricaApplicazione = rndvar {
     let! smartphone = smartphone
-    if not smartphone then return dist { return false }
+    if not smartphone then return dist { return None }
     else
+        let! eta = eta
+        let! fanBieber = fanBieber
+        let! fanChopra = fanChopra
         let! stressed = stressed
+        let! stressLevel = stressLevel
+        let prob = match eta with
+                    | e when e < 10 -> 0.0001 + if fanBieber then 0.1 else 0.0 + if fanChopra then 0.1 else 0.0
+                    | e when e < 15 -> 0.001 + if stressed then 0.01 else 0.0 + if fanBieber then 0.2 else 0.0 + if fanChopra then 0.25 else 0.0
+                    | e when e < 20 -> 0.002 + if stressed then 0.03 else 0.0 + if fanBieber then 0.2 else 0.0 + if fanChopra then 0.25 else 0.0
+                    | e when e < 25 -> 0.003 + if stressed then 0.05 else 0.0 + if fanBieber then 0.25 else 0.0 + if fanChopra then 0.25 else 0.0
+                    | e when e < 35 -> 0.0025 + if stressed then 0.05 else 0.0 + if fanBieber then 0.20 else 0.0 + if fanChopra then 0.25 else 0.0
+                    | e when e < 45 -> 0.0015 + if stressed then 0.1 else 0.0 + if fanBieber then 0.10 else 0.0 + if fanChopra then 0.25 else 0.0
+                    | _ -> 0.001 + if stressed then 0.1 else 0.0 + if fanBieber then 0.10 else 0.0 + if fanChopra then 0.25 else 0.0
         return dist {
             let! u = Dist.uniform
-            return u < if stressed then 0.1 else 0.01
+            return if u < prob then Some eta else None
         }
 }
 
+Dist.getSampleSeq (getDist scaricaApplicazione) (gen())
+    |> Seq.take 3040000
+    |> Seq.filter (function None -> false | _ -> true)
+    |> Seq.countBy (fun x ->5 * ( x.Value /5) )
+    |> Seq.map (fun (x,y) -> x, y*100)
+    |> Chart.Column
 
-Dist.getSampleSeq ( getDist stressed) (gen()) |> Seq.take 1000000 |> Seq.countBy (fun (x:bool) -> if x then "Stressati" else "Tranquilli" ) |> Chart.Pie
+//Dist.getSampleSeq ( getDist stressed) (gen()) |> Seq.take 1000000 |> Seq.countBy (fun (x:bool) -> if x then "Stressati" else "Tranquilli" ) |> Chart.Pie
+
 
 
 let compraPerEta conteggio =
@@ -197,7 +270,7 @@ let compraPerEta conteggio =
         return dist { return compra,eta }
         }
     let totpersoneusa = 304280 * 1000
-    let utenti = dist {
+    let rec utenti rimasti = dist {
         let! compra,eta = getDist compraEta
         return if compra then Some eta else None
         }
